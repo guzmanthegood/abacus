0816f7dd7fd8b819b21eb7adc366c08bcae99423
guzmanweb <carlos@guzmanweb.com>
2016-07-06 10:13:29 +0200

    Add fastclick and slimscroll to application layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,4 +3,6 @@
 //= require turbolinks
 //= require adminlte/bootstrap
 //= require adminlte/app.min
+//= require adminlte/plugins/fastclick/fastclick
+//= require adminlte/plugins/slimScroll/jquery.slimscroll
 //= require_tree .
