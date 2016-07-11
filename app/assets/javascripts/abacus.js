2a02bb161d6a0d3dbd393417abb9d03c6e29b363
guzmanweb <carlos@guzmanweb.com>
2016-07-11 14:07:01 +0200

    fix abacus.js init [#7]

--- app/assets/javascripts/abacus.js
+++ app/assets/javascripts/abacus.js
@@ -1,11 +1,11 @@
 $(document).on('turbolinks:load', function() {
-	//auto_dismissible();
-  //$(document).ajaxStart(function() { Pace.restart(); });
+	auto_dismissible();
+  $(document).ajaxStart(function() { Pace.restart(); });
 });
 
 function auto_dismissible(){
 	$(".auto-dismissible").fadeTo(3000, 500).slideUp(500, function() {
-	    $(".auto-dismissible").alert('close');
+    $(".auto-dismissible").alert('close');
 	});
 }
 
