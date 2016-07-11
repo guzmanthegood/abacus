b55b7a02afeb56aa46230b5c214ccdde8167ca49
guzmanweb <carlos@guzmanweb.com>
2016-07-11 22:16:40 +0000

    Style adjustments, password recovery and edit password screens

--- app/assets/javascripts/login-layout.js
+++
@@ -1,7 +0,0 @@
-//= require jquery
-//= require jquery_ujs
-//= require adminlte/bootstrap
-//= require adminlte/app
-//= require adminlte/plugins/fastclick/fastclick
-//= require adminlte/plugins/slimScroll/jquery.slimscroll
-//= require turbolinks
