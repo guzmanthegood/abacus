19cdd2210eb1f0c3514915ec1300bbf08ca96279
guzmanweb <carlos@guzmanweb.com>
2016-07-22 11:51:00 +0200

    Fix indents [#16]

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -4,18 +4,18 @@ module TasksHelper
   end
 
   def subject_short(subject)
-  	str = subject[0..45]
-  	str << "..." if subject.length > 45
-  	str
+    str = subject[0..45]
+    str << "..." if subject.length > 45
+    str
   end
 
   def progress_bar_color(progress)
-  	if progress.between?(0, 49)
-  		"warning"
-  	elsif progress.between?(50, 99)
-  		"primary"	
-  	else
-  		"success"
-  	end
+    if progress.between?(0, 49)
+      "warning"
+    elsif progress.between?(50, 99)
+      "primary" 
+    else
+      "success"
+    end
   end
 end
