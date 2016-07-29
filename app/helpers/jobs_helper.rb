08f6f62130a667a3aa66fbf34639e49c72da65d7
guzmanweb <carlos@guzmanweb.com>
2016-07-29 09:51:47 +0200

    Add job hours to task index [#20]

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -1,5 +1,5 @@
 module JobsHelper
   def total_hours(jobs)
-  	number_with_precision(jobs.inject(0){|s,i| s + i.hours}, precision: 2)
+  	number_with_precision(jobs.inject(0){|s,i| s + i.hours}, precision: 2, separator: '.')
   end
 end
