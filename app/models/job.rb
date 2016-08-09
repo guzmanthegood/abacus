5de0b05b23e48dc7e6b8f8c8cd2b63571482a053
guzmanweb <carlos@guzmanweb.com>
2016-08-09 12:25:41 +0200

    Add greater than 0 job validation [#20]

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,7 +1,8 @@
 class Job < ApplicationRecord
   default_scope { order('performed_at') }
 
-  validates_presence_of :performed_at, :hours
+  validates_numericality_of :hours, greater_than: 0
+  validates_presence_of :performed_at
 
 	belongs_to :user
 	belongs_to :task
